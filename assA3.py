975df2cef463eb36eff6c7ccf2d90d71f0fd1df7
sanjanamhaske <sanjanamhaske2001@gmail.com>
2022-02-08 23:01:35 +0530

    first commit

--- assA3.py
+++ assA3.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
-dataset = pd.read_csv('C:/Users/rushi/OneDrive/Desktop/Python/data.csv')
+dataset = pd.read_csv('C:/Users/rushi/OneDrive/Desktop/Python/play_tennis.csv')
 dataset
 from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
 dataset['outlook'] = le.fit_transform(dataset.outlook)
 dataset['temp'] = le.fit_transform(dataset.temp)
 dataset['humidity'] = le.fit_transform(dataset.humidity)
-dataset['playgolf'] = le.fit_transform(dataset.playgolf)
+
 x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,5].values
 from sklearn.preprocessing import StandardScaler
--- assA5.py
+++ assA5.py
@@ -1,6 +1,7 @@
+from matplotlib.axis import YAxis
 from sklearn import tree
 clf = tree.DecisionTreeClassifier(criterion = 'entropy')
 clf = clf.fit(X, y)
 tree.plot_tree(clf)
 X_pred = clf.predict(X)
-X_pred == y
+new_var = X_pred == YAxis
